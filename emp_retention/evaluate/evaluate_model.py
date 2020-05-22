87503a0bec05fe5b5cc09c385b3b86da1803bc54
leekokhow <64939887+leekokhow@users.noreply.github.com>
2020-05-22 13:49:04 +0800

    Update due to code quality checks

--- emp_retention/evaluate/evaluate_model.py
+++ emp_retention/evaluate/evaluate_model.py
@@ -9,7 +9,7 @@ def main():
     # retrieve argument configured through script_params in estimator
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", dest='model_name', type=str, 
-            help="Name of the model to retrieve from Workspace")
+        help="Name of the model to retrieve from Workspace")
     args = parser.parse_args()
 
     # Get the current run
@@ -18,13 +18,14 @@ def main():
     # Get metrics from current model and compare with the metrics
     # of the new model. The metrics of the new model can be retrieved
     # from run.parent.get_metrics, which were created in training_model.py
-    metrics = ['Accuracy','Precision','Recall','F1-score']
+    metrics = ['Accuracy', 'Precision', 'Recall', 'F1-score']
     current_metrics = {}
     new_metrics = {}
 
     try:
         workspace = run.experiment.workspace
-        model=Model(workspace, args.model_name) # Get latest version.
+        # Get latest model
+        model = Model(workspace, args.model_name)
 
         for key in metrics:
             current_metrics[key] = float(model.tags.get(key))
@@ -46,12 +47,15 @@ def main():
     # Perform comparison. Just do a simple comparison: If Accuracy improves, proceed next step to register model.
     if(model is not None):
         if(new_metrics['Accuracy'] >= current_metrics['Accuracy']):
-            run.log("Result", "New Accuracy is as good as current, will proceed to register new model.")
+            run.log("Result",
+            "New Accuracy is as good as current, will proceed to register new model.")
         else:
-            run.log("Result", "New Accuracy is worse than current, will not register model. Processing cancelled.")
+            run.log("Result",
+            "New Accuracy is worse than current, will not register model. Processing cancelled.")
             run.parent.cancel()
     else:
-        run.log("Result", "This is the first model, will proceed to register the model.")
+        run.log("Result",
+        "This is the first model, will proceed to register the model.")
 
 
 if __name__ == '__main__':
