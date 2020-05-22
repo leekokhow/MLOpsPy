04378441ed10ba597d379abafbe162239faadfac
leekokhow <64939887+leekokhow@users.noreply.github.com>
2020-05-22 18:06:00 +0800

    Update prep_data.py

--- emp_retention/training/prep_data.py
+++ emp_retention/training/prep_data.py
@@ -14,15 +14,16 @@ def prepare_dataset(dataset, run):
     dataset["salary"] = dataset["salary"].map(salary_map)
 
     # Create dummy variables for department feature
-    dataset = pd.get_dummies(dataset, columns=["department"], drop_first=True)
+    dataset = pd.get_dummies(dataset, columns=["department"], 
+                             drop_first=True)
 
     # Get number of positve and negative examples
     pos = dataset[dataset["left"] == 1].shape[0]
     neg = dataset[dataset["left"] == 0].shape[0]
     run.log('Positive', 'Positive examples = {}'.format(pos))
     run.log('Negative', 'Negative examples = {}'.format(neg))
-    run.log('Proportion',
-    'Proportion of positive to negative examples = {:.2f}%'.format((pos / neg) * 100))
+    run.log('Proportion', 'Proportion of positive to negative \
+    examples = {:.2f}%'.format((pos / neg) * 100))
     return dataset
 
 
@@ -32,7 +33,8 @@ def main():
     parser.add_argument('--raw_data_file', dest='raw_data_file',
                         type=str, help='input raw data in .csv file')
     parser.add_argument('--clean_data_folder', dest='clean_data_folder',
-                        type=str, help='output folder path that stores the clean data file')
+                        type=str, help='output folder path that stores \
+                        the clean data file')
     parser.add_argument('--clean_data_file', dest='clean_data_file',
                         type=str, help='name of the clean data file')
     args = parser.parse_args()
@@ -47,13 +49,13 @@ def main():
     # Clean the dataset to use for model training
     dataset = prepare_dataset(dataset, run)
 
-    # For "output" PipelineData, the folder must be created using 
+    # For "output" PipelineData, the folder must be created using
     # os.makedirs() first, then only can write files into the folder.
     os.makedirs(args.clean_data_folder, exist_ok=True)
-    
+
     # Write the dataset into .csv file for the next step to process
     dataset.to_csv(args.clean_data_folder + args.clean_data_file, index=False)
-    run.log('Output path of clean data', args.clean_data_folder 
+    run.log('Output path of clean data', args.clean_data_folder
             + args.clean_data_file)
 
 
